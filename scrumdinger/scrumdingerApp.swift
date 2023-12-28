d479b587180ccdc81154e8a25a5ec62b4ab493a8
ALBERT JORDAN <albertjordan@mac.com>
2023-12-28 10:01:26 -0800

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Albert Jordan on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
